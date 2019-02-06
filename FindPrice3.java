f4d71a7dbbd9e27badf1cdc774f05c0f02232bdf
Lloyd Napata <29262019+loynaps@users.noreply.github.com>
2019-02-06 16:35:25 +0200

    for loop that uses a compound test for termination

---
+++ FindPrice3.java
@@ -0,0 +1,37 @@
+/*
+
+Program: FindPrice1.java
+Description:This program demonstrates a parallel array
+*/
+import java.util.Scanner;
+public class FindPrice3 {
+    public static void main(String[] args) {
+        final int NUMBER_OF_ITEMS = 10;
+        int[] validValues = {101, 108, 201, 213, 266,
+                             304, 311, 409, 411, 412};
+        double[] prices = {0.29, 1.23, 3.50, 0.69, 6.79,
+                           3.19, 0.99, 0.89, 1.26, 8.00};
+        String strItem;
+        int itemOrdered;
+        double itemPrice = 0.0;
+        boolean validItem = false;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the item number you want to order: ");
+        strItem = input.nextLine();
+        itemOrdered = Integer.parseInt(strItem);
+
+        for (int x = 0; x < NUMBER_OF_ITEMS && !validItem; ++x) {
+            if (itemOrdered == validValues[x]) {
+                validItem = true;
+                itemPrice = prices[x];
+                //A for loop that uses a compound test for termination
+            }
+        }
+        if (validItem)
+            System.out.println("The price for item "
+                               + itemOrdered
+                               + " is $" + itemPrice);
+        else
+            System.out.println("Sorry - invalid item entered");
+    }
+}
